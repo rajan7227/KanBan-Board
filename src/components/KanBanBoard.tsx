2dff3ec83ff870cb15db23cb6ec2a0b32a718dff
rajan7227 <114588581+rajan7227@users.noreply.github.com>
2023-10-27 17:05:18 +0100

    fixed the index problem when moving the columns

--- src/components/KanBanBoard.tsx
+++ src/components/KanBanBoard.tsx
@@ -2,8 +2,8 @@ import { PlusIcon } from "../icons/Plusicon";
 import { useState, useMemo } from "react";
 import { Column, Id, Task } from "../types";
 import { ColumnContainer } from "./ColumnContainer";
-import { DndContext, DragOverlay, DragStartEvent } from "@dnd-kit/core";
-import { SortableContext } from "@dnd-kit/sortable";
+import { DndContext, DragEndEvent, DragOverlay, DragStartEvent } from "@dnd-kit/core";
+import { SortableContext, arrayMove } from "@dnd-kit/sortable";
 import { createPortal } from "react-dom";
 
 function KanBanBoard() {
@@ -14,7 +14,7 @@ function KanBanBoard() {
 
   return (
     <div className="m-auto flex min-h-screen w-full items-center  overflow-x-auto overflow-y-hidden px-[40px]">
-      <DndContext onDragStart={onDragStart}>
+      <DndContext onDragStart={onDragStart} onDragEnd={onDragEnd}>
         <div className="m-auto flex gap-4">
           <div className="flex  gap-4 ">
             <SortableContext items={columnsId}>
@@ -81,6 +81,22 @@ function KanBanBoard() {
       setActiveColumn(event.active.data.current.column);
       return;
     }
+  }
+
+  function onDragEnd (event: DragEndEvent) {
+    const { active, over } = event;
+    if (!over) return;
+    const activeColumnId = active.id;
+    const overColumnId = over.id;
+
+    if( activeColumnId === overColumnId) return;
+   
+    const activeColumnIndex = columns.findIndex((col) => col.id === activeColumnId);
+    const overColumnIndex = columns.findIndex((col)=> col.id === overColumnId);
+
+    const updatedColumns = arrayMove(columns, activeColumnIndex, overColumnIndex);
+
+    setColumns(updatedColumns);
   }
 }
 
